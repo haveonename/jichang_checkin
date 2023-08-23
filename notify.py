72fc0d0da945af6552767624cfab4f8cc5506350
haveonename <39997350+haveonename@users.noreply.github.com>
2023-08-23 12:12:54 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -135,6 +135,9 @@ def dingding_bot(title: str, content: str) -> None:
     """
     使用 钉钉机器人 推送消息。
     """
+    # 钉钉
+    DD_TOKEN = os.environ.get('DD_TOKEN')
+    DD_SECRET = os.environ.get('DD_SECRET')
     if not push_config.get("DD_BOT_SECRET") or not push_config.get("DD_BOT_TOKEN"):
         print("钉钉机器人 服务的 DD_BOT_SECRET 或者 DD_BOT_TOKEN 未设置!!\n取消推送")
         return
@@ -143,14 +146,14 @@ def dingding_bot(title: str, content: str) -> None:
     print("钉钉机器人 服务启动")
 
     timestamp = str(round(time.time() * 1000))
-    secret_enc = push_config.get("DD_BOT_SECRET").encode("utf-8")
-    string_to_sign = "{}\n{}".format(timestamp, push_config.get("DD_BOT_SECRET"))
+    secret_enc = DD_SECRET.encode("utf-8")
+    string_to_sign = "{}\n{}".format(timestamp, DD_SECRET)
     string_to_sign_enc = string_to_sign.encode("utf-8")
     hmac_code = hmac.new(
         secret_enc, string_to_sign_enc, digestmod=hashlib.sha256
     ).digest()
     sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-    url = f'https://oapi.dingtalk.com/robot/send?access_token={push_config.get("DD_BOT_TOKEN")}&timestamp={timestamp}&sign={sign}'
+    url = f'https://oapi.dingtalk.com/robot/send?access_token={DD_TOKEN}&timestamp={timestamp}&sign={sign}'
     headers = {"Content-Type": "application/json;charset=utf-8"}
     data = {"msgtype": "text", "text": {"content": f"{title}\n\n{content}"}}
     response = requests.post(
